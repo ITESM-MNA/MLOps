9642fa31376681739d06b7c1c5881f1638730295
Gerardo Rodriguez Hernández <gerardo.rodriguez@tec.mx>
2024-09-27 14:34:42 -0600

    Changes Activity 1.3

---
+++ notebooks/1.3_Modeling_pipeline/mlflow_test.py
@@ -0,0 +1,91 @@
+import os
+import warnings
+import sys
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+from mlflow.models import infer_signature
+
+
+import logging
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
+if __name__ == "__main__": 
+    mlflow.set_tracking_uri('http://localhost:5000')
+    experiment = mlflow.set_experiment("sklearn-wine")
+    print("mlflow tracking uri:", mlflow.tracking.get_tracking_uri())
+    print("experiment:", experiment)
+    warnings.filterwarnings("ignore")
+    # np.random.seed(40)
+
+    # Read the wine-quality csv file from the URL
+    csv_url = (
+        "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+    )
+    try:
+        data = pd.read_csv(csv_url, sep=";")
+    except Exception as e:
+        logger.exception(
+            "Unable to download training & test CSV, check your internet connection. Error: %s", e
+        )
+
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train, test = train_test_split(data)
+
+    # The predicted column is "quality" which is a scalar from [3, 9]
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+    with mlflow.start_run(experiment_id=experiment.experiment_id):
+        
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio)
+        lr.fit(train_x, train_y)
+
+        predicted_qualities = lr.predict(test_x)
+
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
+
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        signature = infer_signature(test_x, lr.predict(test_x))
+
+
+        model_info = mlflow.sklearn.log_model(
+            sk_model=lr, artifact_path="model", signature=signature)
+        print("Saved model!", model_info.model_uri)
+
+        sklearn_pyfunc = mlflow.pyfunc.load_model(model_uri=model_info.model_uri)
+        # data = np.array([-4, 1, 0, 10, -2, 1]).reshape(-1, 1)
+        predictions = sklearn_pyfunc.predict(test_x)
+        print("Preds: ", predictions[0:10])
