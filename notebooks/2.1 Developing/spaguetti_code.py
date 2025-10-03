cd74152345d0bb6cf07c671b9a31fdc1542a0e01
Liobreaker1 <richardvh01@gmail.com>
2025-10-02 19:37:20 -0600

    feat: Modifying Wine EDA notebook, and including refactored versions in notebook for explainability

---
+++ notebooks/2.1 Developing/spaguetti_code.py
@@ -0,0 +1,14 @@
+# Spaguetti Code
+def process_data():
+  d = [34, 45, 76, 54, 98]
+
+  d1 = []
+  for x in d:
+    d1.append(x ** 2)
+  d2 = []
+  for x in d:
+    d2.append(x ** 3)
+
+  d3 = []
+  for x in d:
+    d3.append(x ** 0.5)
